1c6154dcbddadea283cac4d3dcb5a9e58c8ca35c
Sora <34794115+sorae42@users.noreply.github.com>
2024-06-25 21:19:48 +0700

    feat: layout skeleton

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
+import vi from '@angular/common/locales/vi';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(vi);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    { provide: LOCALE_ID, useValue: 'vi-VN' },
+  ],
 };
