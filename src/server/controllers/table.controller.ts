8bddc2a19233a54ce512f7c1d42fca04cc80f1cc
Sora <34794115+sorae42@users.noreply.github.com>
2024-06-27 12:11:53 +0700

    more api

--- src/server/controllers/table.controller.ts
+++ src/server/controllers/table.controller.ts
@@ -32,4 +32,47 @@ router.post('/create', async (req, res) => {
   }
 });
 
+router.post('/edit', async (req, res) => {
+  if (!req.body.id || !req.body.name) {
+    res.status(400);
+    return res.json({ message: 'Table ID is missing or name is not specified!!' });
+  }
+
+  try {
+    const table = await DI.table.findOne({ id: req.body.id });
+    if (!table) {
+      res.status(400);
+      return res.json({ message: 'Table not found!' });
+    }
+
+    table.display_name = req.body.name;
+    await DI.em.flush();
+
+    res.json(table);
+  } catch (e: any) {
+    return res.status(400).json({ message: e.message });
+  }
+});
+
+router.post('/delete', async (req, res) => {
+  if (!req.body.id) {
+    res.status(400);
+    return res.json({ message: 'Table ID is missing!!' });
+  }
+
+  try {
+    const table = await DI.table.findOne({ id: req.body.id });
+    if (!table) {
+      res.status(400);
+      return res.json({ message: 'Table not found!' });
+    }
+
+    await DI.table.nativeDelete(table);
+
+    res.json(table);
+  } catch (e: any) {
+    return res.status(400).json({ message: e.message });
+  }
+})
+
 export const TableController = router;
